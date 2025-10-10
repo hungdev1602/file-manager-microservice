46f9f460c57a02a10645e826e8e406ebad495640
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-10-10 21:22:44 +0300

    Xây dựng hệ thống lưu trữ file độc lập - File Manager

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, Express } from "express";
+import express, { Express } from "express";
+import routes from "./routes/index.route"
 
 const app: Express = express()
 const port: number = 4000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!")
-})
+app.use("/", routes)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
