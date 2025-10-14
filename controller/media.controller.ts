f3e13b1c334606d7a3b68380dc3950c8d73127ef
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-10-15 01:16:18 +0300

    tính năng download file

--- controller/media.controller.ts
+++ controller/media.controller.ts
@@ -3,8 +3,14 @@ import path from "path"
 
 export const getFile = (req: Request, res: Response) => {
   const filename = req.params.filename
+  const type = req.query.type
 
   const mediaPath = path.join(__dirname, "../media", filename) // đường dẫn đến file
 
-  res.sendFile(mediaPath)
+  if(type === "download"){
+    res.download(mediaPath)
+  }
+  else{
+    res.sendFile(mediaPath)
+  }
 }
